cb08214bc8c711b2721e5887b4daac48e0fc2754
chapmanap25 <chapmanap25@gmail.com>
2021-04-07 16:16:48 -0500

    Update for Lab 4

--- trip.js
+++ trip.js
@@ -1,32 +1,48 @@
+$(document).ready(function () {
+    $("button").on("click", calculate);
+});
+
+
 function calculate()
 {
     // gather all input
 
     // ask user for number of pizza
     // convert to number type
-    let totalPizza = parseFloat( prompt("How many pizzas?") );
+    let totalPizza = parseFloat( $("#totalPizza").val() );
+
+    //totalPizza = parseFloat(totalPizza);
 
     // ask user for number of toppings
     // convert to number type
-    let pizzaToppings = parseFloat( prompt("How many total toppings?") );
+    let pizzaToppings = parseFloat( $("#pizzaToppings").val() );
 
     // ask user for number of coworkers
     // convert to number type
-    let totalCoworkers = parseFloat( prompt("How many coworkers are pitching in?") );
+    let totalCoworkers = parseFloat( $("#totalCoworkers").val() );
+
 
     // do some math
 
     // multiple number of pizzas by 15
     // multiple number of toppings by 1.25
     // divide total cost by number of coworkers
-    let totalCost = (15 * totalPizza)+(1.25 * pizzaToppings);
+
+    // multiple number of pizzas by 15
+    let totalCost = (totalPizza * 15) + (pizzaToppings * 1.25);
+
+    // divide total cost by number of coworkers
     let individualCost = (totalCost / totalCoworkers);
 
     // OUTPUT!
 
-    // alert the gas cost
-    alert(`The total cost is $${totalCost.toFixed(2)} `);
+    // alert the total cost
+    // alert(`The pizza bill will be $${totalCost.toFixed(2)}`);
+    $("span#totalCost").text(totalCost.toFixed(2));
 
-    // alert the hours
-    alert(`The cost per person is $${individualCost.toFixed(2)}`);
+    // alert the individual cost
+    // alert(`Each coworker will pay ${individualCost.toFixed(2)}`);
+    $("span#individualCost").text(individualCost.toFixed(2));
+
+    $(".output").show();
 }
